e0dddb58ed814e38ce7c6d57439bf90dc16bdbf0
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-02-21 22:08:47 +0300

    Update appId.js

--- src/external/bot-skeleton/services/api/appId.js
+++ src/external/bot-skeleton/services/api/appId.js
@@ -7,7 +7,7 @@ import APIMiddleware from './api-middleware';
 export const generateDerivApiInstance = () => {
     const cleanedServer = getSocketURL().replace(/[^a-zA-Z0-9.]/g, '');
     const cleanedAppId = '68848';
-    const socket_url = `wss://${cleanedServer}/websockets/v3?app_id=${cleanedAppId}&l=${getInitialLanguage()}&brand=${website_name.toLowerCase()}`;
+    const socket_url = `wss://ws.binaryws.com/websockets/v3?app_id=${cleanedAppId}&l=${getInitialLanguage()}&brand=${website_name.toLowerCase()}`;
     const deriv_socket = new WebSocket(socket_url);
     const deriv_api = new DerivAPIBasic({
         connection: deriv_socket,
