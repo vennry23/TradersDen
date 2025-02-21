66ce2a304093fc30317e67da7e00989b7c8d5e3f
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-02-22 01:29:16 +0300

    Update site-config.ts

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
 export const website_domain = 'bot.binaryfx.site';
-export const website_name = 'Deriv';
+export const website_name = 'Binaryfx';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
