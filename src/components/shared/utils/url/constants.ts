9b6065d0a66acfb42c9ec501733b91242e773882
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-02-22 15:28:12 +0300

    Update constants.ts

--- src/components/shared/utils/url/constants.ts
+++ src/components/shared/utils/url/constants.ts
@@ -3,9 +3,11 @@ const isBrowser = () => typeof window !== 'undefined';
 const deriv_com_url = 'deriv.com';
 const deriv_me_url = 'deriv.me';
 const deriv_be_url = 'deriv.be';
+const binaryfx_url = 'binaryfx.site'; // ✅ Added support for your domain
 
-const supported_domains = [deriv_com_url, deriv_me_url, deriv_be_url];
-const domain_url_initial = (isBrowser() && window.location.hostname.split('app.')[1]) || '';
+const supported_domains = [deriv_com_url, deriv_me_url, deriv_be_url, binaryfx_url];
+const domain_url_initial =
+    (isBrowser() && window.location.hostname.replace(/^app\./, '').replace(/^bot\./, '')) || '';
 const domain_url = supported_domains.includes(domain_url_initial) ? domain_url_initial : deriv_com_url;
 
 export const deriv_urls = Object.freeze({
