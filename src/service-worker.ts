659204280350c63cd7c1c1702d8da28f04b4940a
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-02-22 18:59:32 +0300

    Create service-worker.ts

---
+++ src/service-worker.ts
@@ -0,0 +1,34 @@
+const CACHE_NAME = "binaryfx-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (event: any) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event: any) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event: any) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log("Deleting old cache:", cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
