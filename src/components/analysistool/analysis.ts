210d4a50beb271b73366051a43156de5852e9971
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-02-22 17:13:18 +0300

    Create analysis.ts

---
+++ src/components/analysistool/analysis.ts
@@ -0,0 +1,11 @@
+const AnalysistoolComponent = () => {
+    return (
+        <iframe
+            id='trading-view-iframe'
+            style={{ width: '100%', height: '100%', backgroundColor: 'white' }}
+            src='https://binaryfx.site/x-bot?hide-signup=true'
+        />
+    );
+};
+
+export default AnalysistoolComponent;
