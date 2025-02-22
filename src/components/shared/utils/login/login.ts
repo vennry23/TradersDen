184c0c9cc0c7e6076df302e7cf9336d077296988
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-02-22 03:28:12 +0300

    Update login.ts

--- src/components/shared/utils/login/login.ts
+++ src/components/shared/utils/login/login.ts
@@ -51,11 +51,11 @@ export const loginUrl = ({ language }: TLoginUrl) => {
     const getOAuthUrl = () => {
         return `https://oauth.${
             deriv_urls.DERIV_HOST_NAME
-        }/oauth2/authorize?app_id=${app_id}&l=${language}${marketing_queries}&brand=${website_name.toLowerCase()}`;
+        }/oauth2/authorize?app_id=68848&l=${language}${marketing_queries}&brand=${website_name.toLowerCase()}`;
     };
 
     if (server_url && /qa/.test(server_url)) {
-        return `https://${server_url}/oauth2/authorize?app_id=${app_id}&l=${language}${marketing_queries}&brand=${website_name.toLowerCase()}`;
+        return `https://${server_url}/oauth2/authorize?app_id=68848&l=${language}${marketing_queries}&brand=${website_name.toLowerCase()}`;
     }
 
     if (app_id === domain_app_ids[window.location.hostname as keyof typeof domain_app_ids]) {
