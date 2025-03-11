f6dd7ef9f56f3120d77cacb04fc2857d9c2f5393
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-03-12 02:00:56 +0300

    Update header.tsx

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -12,97 +12,21 @@ import { Localize, useTranslations } from '@deriv-com/translations';
 import { Header, useDevice, Wrapper } from '@deriv-com/ui';
 import { Tooltip } from '@deriv-com/ui';
 import { AppLogo } from '../app-logo';
-import AccountsInfoLoader from './account-info-loader';
-import AccountSwitcher from './account-switcher';
-import MenuItems from './menu-items';
 import MobileMenu from './mobile-menu';
-import PlatformSwitcher from './platform-switcher';
 import './header.scss';
 
 const TelegramIcon = () => (
     <a href="https://t.me/binary_fx" target="_blank" rel="noopener noreferrer" className="telegram-icon">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9.00002 14.175L9.00002 17.885C9.00002 18.37 9.54002 18.615 9.90002 18.295L11.77 16.62L15.36 19.265C15.915 19.66 16.69 19.325 16.855 18.665L19.92 6.80501C20.105 6.04001 19.34 5.43501 18.67 5.75501L4.42002 12.675C3.75002 12.995 3.80002 13.98 4.49502 14.225L9.00002 14.175Z" fill="#0088cc"/>
-        </svg>
+        <svg width="256px" height="256px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="SVGRepo_bgCarrier" stroke-width="0"/>
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+<g id="SVGRepo_iconCarrier"> <circle cx="16" cy="16" r="14" fill="url(#paint0_linear_87_7225)"/> <path d="M22.9866 10.2088C23.1112 9.40332 22.3454 8.76755 21.6292 9.082L7.36482 15.3448C6.85123 15.5703 6.8888 16.3483 7.42147 16.5179L10.3631 17.4547C10.9246 17.6335 11.5325 17.541 12.0228 17.2023L18.655 12.6203C18.855 12.4821 19.073 12.7665 18.9021 12.9426L14.1281 17.8646C13.665 18.3421 13.7569 19.1512 14.314 19.5005L19.659 22.8523C20.2585 23.2282 21.0297 22.8506 21.1418 22.1261L22.9866 10.2088Z" fill="white"/> <defs> <linearGradient id="paint0_linear_87_7225" x1="16" y1="2" x2="16" y2="30" gradientUnits="userSpaceOnUse"> <stop stop-color="#37BBFE"/> <stop offset="1" stop-color="#007DBB"/> </linearGradient> </defs> </g>
+</svg>
     </a>
 );
 
 const AppHeader = observer(() => {
     const { isDesktop } = useDevice();
-    const { isAuthorizing, activeLoginid } = useApiBase();
-    const { client } = useStore() ?? {};
-
-    const { data: activeAccount } = useActiveAccount({ allBalanceData: client?.all_accounts_balance });
-    const { accounts } = client ?? {};
-    const has_wallet = Object.keys(accounts ?? {}).some(id => accounts?.[id].account_category === 'wallet');
-
-    const { localize } = useTranslations();
-
-    const { isOAuth2Enabled } = useOauth2();
-
-    const renderAccountSection = () => {
-        if (isAuthorizing) {
-            return <AccountsInfoLoader isLoggedIn isMobile={!isDesktop} speed={3} />;
-        } else if (activeLoginid) {
-            return (
-                <>
-                    {isDesktop && (
-                        <Tooltip
-                            as='a'
-                            href={standalone_routes.personal_details}
-                            tooltipContent={localize('Manage account settings')}
-                            tooltipPosition='bottom'
-                            className='app-header__account-settings'
-                        >
-                            <StandaloneCircleUserRegularIcon className='app-header__profile_icon' />
-                        </Tooltip>
-                    )}
-                    <AccountSwitcher activeAccount={activeAccount} />
-                    {isDesktop &&
-                        (has_wallet ? (
-                            <Button
-                                className='manage-funds-button'
-                                has_effect
-                                text={localize('Manage funds')}
-                                onClick={() => window.location.assign(standalone_routes.wallets_transfer)}
-                                primary
-                            />
-                        ) : (
-                            <Button
-                                primary
-                                onClick={() => {
-                                    window.location.assign(standalone_routes.cashier_deposit);
-                                }}
-                                className='deposit-button'
-                            >
-                                {localize('Deposit')}
-                            </Button>
-                        ))}
-                </>
-            );
-        } else {
-            return (
-                <div className='auth-actions'>
-                    <Button
-                        tertiary
-                        onClick={() => {
-                            window.location.replace('https://oauth.deriv.com/oauth2/authorize?app_id=68848&l=EN&brand=binaryfx');
-                        }}
-                    >
-                        <Localize i18n_default_text='Log in' />
-                    </Button>
-                    <Button
-                        primary
-                        onClick={() => {
-                            window.open(standalone_routes.signup);
-                        }}
-                    >
-                        <Localize i18n_default_text='Sign up' />
-                    </Button>
-                </div>
-            );
-        }
-    };
 
     return (
         <Header
@@ -113,13 +37,9 @@ const AppHeader = observer(() => {
         >
             <Wrapper variant='left'>
                 <AppLogo />
-                <TelegramIcon />
                 <MobileMenu />
-                {isDesktop && <MenuItems.TradershubLink />}
-                {isDesktop && <PlatformSwitcher />}
-                {isDesktop && <MenuItems />}
+                {!isDesktop && <TelegramIcon />}
             </Wrapper>
-            <Wrapper variant='right'>{renderAccountSection()}</Wrapper>
         </Header>
     );
 });
