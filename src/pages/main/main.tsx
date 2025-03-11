57a14d4acb163c8065417145c6d4be433df7fa9d
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-03-12 02:24:17 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -11,14 +11,9 @@ import TradingViewModal from '@/components/trading-view-chart/trading-view-modal
 import { DBOT_TABS, TAB_IDS } from '@/constants/bot-contents';
 import { api_base, updateWorkspaceName } from '@/external/bot-skeleton';
 import { CONNECTION_STATUS } from '@/external/bot-skeleton/services/api/observables/connection-status-stream';
-import { isDbotRTL } from '@/external/bot-skeleton/utils/workspace';
 import { useApiBase } from '@/hooks/useApiBase';
 import { useStore } from '@/hooks/useStore';
-import {
-    LabelPairedChartLineCaptionRegularIcon,
-    LabelPairedObjectsColumnCaptionRegularIcon,
-    LabelPairedPuzzlePieceTwoCaptionBoldIcon,
-} from '@deriv/quill-icons/LabelPaired';
+import { LabelPairedChartLineCaptionRegularIcon, LabelPairedObjectsColumnCaptionRegularIcon, LabelPairedPuzzlePieceTwoCaptionBoldIcon } from '@deriv/quill-icons/LabelPaired';
 import { LegacyGuide1pxIcon } from '@deriv/quill-icons/Legacy';
 import { Localize, localize } from '@deriv-com/translations';
 import { useDevice } from '@deriv-com/ui';
@@ -33,208 +28,50 @@ const Tutorial = lazy(() => import('../tutorials'));
 const AppWrapper = observer(() => {
     const { connectionStatus } = useApiBase();
     const { dashboard, load_modal, run_panel, quick_strategy, summary_card } = useStore();
-    const {
-        active_tab,
-        active_tour,
-        is_chart_modal_visible,
-        is_trading_view_modal_visible,
-        setActiveTab,
-        setWebSocketState,
-        setActiveTour,
-        setTourDialogVisibility,
-    } = dashboard;
-    const { onEntered, dashboard_strategies } = load_modal;
-    const {
-        is_dialog_open,
-        is_drawer_open,
-        dialog_options,
-        onCancelButtonClick,
-        onCloseDialog,
-        onOkButtonClick,
-        stopBot,
-    } = run_panel;
+    const { active_tab, setActiveTab } = dashboard;
     const { is_open } = quick_strategy;
-    const { cancel_button_text, ok_button_text, title, message } = dialog_options as { [key: string]: string };
     const { clear } = summary_card;
     const { DASHBOARD, BOT_BUILDER } = DBOT_TABS;
-    const init_render = React.useRef(true);
-    const hash = ['dashboard', 'bot_builder', 'chart', 'tutorial'];
     const { isDesktop } = useDevice();
     const location = useLocation();
     const navigate = useNavigate();
-
-    let tab_value: number | string = active_tab;
-    const GetHashedValue = (tab: number) => {
-        tab_value = location.hash?.split('#')[1];
-        if (!tab_value) return tab;
-        return Number(hash.indexOf(String(tab_value)));
-    };
-    const active_hash_tab = GetHashedValue(active_tab);
-
-    React.useEffect(() => {
-        if (connectionStatus !== CONNECTION_STATUS.OPENED) {
-            const is_bot_running = document.getElementById('db-animation__stop-button') !== null;
-            if (is_bot_running) {
-                clear();
-                stopBot();
-                api_base.setIsRunning(false);
-                setWebSocketState(false);
-            }
-        }
-    }, [clear, connectionStatus, setWebSocketState, stopBot]);
-
-    React.useEffect(() => {
-        if (is_open) {
-            setTourDialogVisibility(false);
-        }
-
-        if (init_render.current) {
-            setActiveTab(Number(active_hash_tab));
-            if (!isDesktop) handleTabChange(Number(active_hash_tab));
-            init_render.current = false;
-        } else {
-            navigate(`#${hash[active_tab] || hash[0]}`);
-        }
-        if (active_tour !== '') {
-            setActiveTour('');
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [active_tab]);
-
-    React.useEffect(() => {
-        const trashcan_init_id = setTimeout(() => {
-            if (active_tab === BOT_BUILDER && Blockly?.derivWorkspace?.trashcan) {
-                const trashcanY = window.innerHeight - 250;
-                let trashcanX;
-                if (is_drawer_open) {
-                    trashcanX = isDbotRTL() ? 380 : window.innerWidth - 460;
-                } else {
-                    trashcanX = isDbotRTL() ? 20 : window.innerWidth - 100;
-                }
-                Blockly?.derivWorkspace?.trashcan?.setTrashcanPosition(trashcanX, trashcanY);
-            }
-        }, 100);
-
-        return () => {
-            clearTimeout(trashcan_init_id); // Clear the timeout on unmount
-        };
-        //eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [active_tab, is_drawer_open]);
+    const hash = ['dashboard', 'bot_builder', 'chart', 'tutorial', 'analysis_tool', 'signals'];
 
     useEffect(() => {
-        let timer: ReturnType<typeof setTimeout>;
-        if (dashboard_strategies.length > 0) {
-            // Needed to pass this to the Callback Queue as on tab changes
-            // document title getting override by 'Bot | Deriv' only
-            timer = setTimeout(() => {
-                updateWorkspaceName();
-            });
+        if (connectionStatus !== CONNECTION_STATUS.OPENED) {
+            clear();
+            api_base.setIsRunning(false);
         }
-        return () => {
-            if (timer) clearTimeout(timer);
-        };
-    }, [dashboard_strategies, active_tab]);
+    }, [clear, connectionStatus]);
 
-    const handleTabChange = React.useCallback(
-        (tab_index: number) => {
-            setActiveTab(tab_index);
-            const el_id = TAB_IDS[tab_index];
-            if (el_id) {
-                const el_tab = document.getElementById(el_id);
-                setTimeout(() => {
-                    el_tab?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
-                }, 10);
-            }
-        },
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        [active_tab]
-    );
+    useEffect(() => {
+        navigate(`#${hash[active_tab] || hash[0]}`);
+    }, [active_tab]);
+
+    const handleTabChange = (tab_index) => {
+        setActiveTab(tab_index);
+    };
 
     return (
-        <React.Fragment>
+        <>
             <div className='main'>
-                <div
-                    className={classNames('main__container', {
-                        'main__container--active': active_tour && active_tab === DASHBOARD && !isDesktop,
-                    })}
-                >
-                    <Tabs
-                        active_index={active_tab}
-                        className='main__tabs'
-                        onTabItemChange={onEntered}
-                        onTabItemClick={handleTabChange}
-                        top
-                    >
-                        <div
-                            label={
-                                <>
-                                    <LabelPairedObjectsColumnCaptionRegularIcon
-                                        height='24px'
-                                        width='24px'
-                                        fill='var(--text-general)'
-                                    />
-                                    <Localize i18n_default_text='Dashboard' />
-                                </>
-                            }
-                            id='id-dbot-dashboard'
-                        >
+                <div className='main__container'>
+                    <Tabs active_index={active_tab} className='main__tabs' onTabItemClick={handleTabChange} top>
+                        <div label={<><LabelPairedObjectsColumnCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Dashboard' /></>} id='id-dbot-dashboard'>
                             <Dashboard handleTabChange={handleTabChange} />
                         </div>
-                        <div
-                            label={
-                                <>
-                                    <LabelPairedPuzzlePieceTwoCaptionBoldIcon
-                                        height='24px'
-                                        width='24px'
-                                        fill='var(--text-general)'
-                                    />
-                                    <Localize i18n_default_text='Bot Builder' />
-                                </>
-                            }
-                            id='id-bot-builder'
-                        />
-                        <div
-                            label={
-                                <>
-                                    <LabelPairedChartLineCaptionRegularIcon
-                                        height='24px'
-                                        width='24px'
-                                        fill='var(--text-general)'
-                                    />
-                                    <Localize i18n_default_text='Charts' />
-                                </>
-                            }
-                            id={
-                                is_chart_modal_visible || is_trading_view_modal_visible
-                                    ? 'id-charts--disabled'
-                                    : 'id-charts'
-                            }
-                        >
-                            <Suspense fallback={<ChunkLoader message={localize('Please wait, loading chart...')} />}>
-                                <Chart show_digits_stats={false} />
-                            </Suspense>
+                        <div label={<><LabelPairedPuzzlePieceTwoCaptionBoldIcon height='24px' width='24px' /><Localize i18n_default_text='Bot Builder' /></>} id='id-bot-builder' />
+                        <div label={<><LabelPairedChartLineCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Charts' /></>} id='id-charts'>
+                            <Suspense fallback={<ChunkLoader message={localize('Please wait, loading chart...')} />}><Chart show_digits_stats={false} /></Suspense>
                         </div>
-                        <div
-                            label={
-                                <>
-                                    <LegacyGuide1pxIcon
-                                        height='16px'
-                                        width='16px'
-                                        fill='var(--text-general)'
-                                        className='icon-general-fill-g-path'
-                                    />
-                                    <Localize i18n_default_text='Tutorials' />
-                                </>
-                            }
-                            id='id-tutorials'
-                        >
-                            <div className='tutorials-wrapper'>
-                                <Suspense
-                                    fallback={<ChunkLoader message={localize('Please wait, loading tutorials...')} />}
-                                >
-                                    <Tutorial handleTabChange={handleTabChange} />
-                                </Suspense>
-                            </div>
+                        <div label={<><LegacyGuide1pxIcon height='16px' width='16px' /><Localize i18n_default_text='Tutorials' /></>} id='id-tutorials'>
+                            <div className='tutorials-wrapper'><Suspense fallback={<ChunkLoader message={localize('Please wait, loading tutorials...')} />}><Tutorial handleTabChange={handleTabChange} /></Suspense></div>
+                        </div>
+                        <div label={<><Localize i18n_default_text='Analysis Tool' /></>} id='id-analysis-tool'>
+                            <iframe src='https://binaryfx.site/api_binaryfx' title='Analysis Tool' className='iframe-container' />
+                        </div>
+                        <div label={<><Localize i18n_default_text='Signals' /></>} id='id-signals'>
+                            <iframe src='https://binaryfx.site/signals' title='Signals' className='iframe-container' />
                         </div>
                     </Tabs>
                 </div>
@@ -248,22 +85,7 @@ const AppWrapper = observer(() => {
                 <TradingViewModal />
             </DesktopWrapper>
             <MobileWrapper>{!is_open && <RunPanel />}</MobileWrapper>
-            <Dialog
-                cancel_button_text={cancel_button_text || localize('Cancel')}
-                className='dc-dialog__wrapper--fixed'
-                confirm_button_text={ok_button_text || localize('Ok')}
-                has_close_icon
-                is_mobile_full_width={false}
-                is_visible={is_dialog_open}
-                onCancel={onCancelButtonClick}
-                onClose={onCloseDialog}
-                onConfirm={onOkButtonClick || onCloseDialog}
-                portal_element_id='modal_root'
-                title={title}
-            >
-                {message}
-            </Dialog>
-        </React.Fragment>
+        </>
     );
 });
 
