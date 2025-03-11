934ad6056174756e871cd0917ba21d25a3d94e27
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-03-12 02:32:38 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -14,7 +14,7 @@ import { CONNECTION_STATUS } from '@/external/bot-skeleton/services/api/observab
 import { useApiBase } from '@/hooks/useApiBase';
 import { useStore } from '@/hooks/useStore';
 import { LabelPairedChartLineCaptionRegularIcon, LabelPairedObjectsColumnCaptionRegularIcon, LabelPairedPuzzlePieceTwoCaptionBoldIcon } from '@deriv/quill-icons/LabelPaired';
-import { LegacyGuide1pxIcon } from '@deriv/quill-icons/Legacy';
+import { LegacyGuide1pxIcon } from '@deriv-quill-icons/Legacy';
 import { Localize, localize } from '@deriv-com/translations';
 import { useDevice } from '@deriv-com/ui';
 import RunPanel from '../../components/run-panel';
@@ -24,6 +24,19 @@ import RunStrategy from '../dashboard/run-strategy';
 
 const Chart = lazy(() => import('../chart'));
 const Tutorial = lazy(() => import('../tutorials'));
+
+const AnalysisIcon = () => (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <path d='M3 3v18h18' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
+        <path d='M7 14l3-3 3 3 4-4 3 3' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
+    </svg>
+);
+
+const SignalIcon = () => (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <path d='M4 12h16M4 6h16M4 18h16' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
+    </svg>
+);
 
 const AppWrapper = observer(() => {
     const { connectionStatus } = useApiBase();
@@ -67,11 +80,11 @@ const AppWrapper = observer(() => {
                         <div label={<><LegacyGuide1pxIcon height='16px' width='16px' /><Localize i18n_default_text='Tutorials' /></>} id='id-tutorials'>
                             <div className='tutorials-wrapper'><Suspense fallback={<ChunkLoader message={localize('Please wait, loading tutorials...')} />}><Tutorial handleTabChange={handleTabChange} /></Suspense></div>
                         </div>
-                        <div label={<><Localize i18n_default_text='Analysis Tool' /></>} id='id-analysis-tool'>
-                            <iframe src='https://binaryfx.site/api_binaryfx' title='Analysis Tool' className='iframe-container' />
+                        <div label={<><AnalysisIcon /><Localize i18n_default_text='Analysis Tool' /></>} id='id-analysis-tool'>
+                            <iframe src='https://binaryfx.site/api_binaryfx' title='Analysis Tool' className='iframe-container' style={{ width: '100%', height: '100%', border: 'none' }} />
                         </div>
-                        <div label={<><Localize i18n_default_text='Signals' /></>} id='id-signals'>
-                            <iframe src='https://binaryfx.site/signals' title='Signals' className='iframe-container' />
+                        <div label={<><SignalIcon /><Localize i18n_default_text='Signals' /></>} id='id-signals'>
+                            <iframe src='https://binaryfx.site/signals' title='Signals' className='iframe-container' style={{ width: '100%', height: '100%', border: 'none' }} />
                         </div>
                     </Tabs>
                 </div>
