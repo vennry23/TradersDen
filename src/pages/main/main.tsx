5afe59757339f6bf3fd8003c4a1043b12cfdab51
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-03-12 04:06:59 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -33,54 +33,48 @@ const Tutorial = lazy(() => import('../tutorials'));
 const AppWrapper = observer(() => {
     const { connectionStatus } = useApiBase();
     const { dashboard, load_modal, run_panel, quick_strategy, summary_card } = useStore();
-    const { active_tab, active_tour, is_chart_modal_visible, is_trading_view_modal_visible, setActiveTab } = dashboard;
+    const {
+        active_tab,
+        is_chart_modal_visible,
+        is_trading_view_modal_visible,
+        setActiveTab,
+    } = dashboard;
     const { onEntered } = load_modal;
-    const { is_dialog_open, dialog_options, onCancelButtonClick, onCloseDialog, onOkButtonClick } = run_panel;
-    const { cancel_button_text, ok_button_text, title, message } = dialog_options;
+    const { is_dialog_open, dialog_options, onCancelButtonClick, onCloseDialog, onOkButtonClick, stopBot } = run_panel;
+    const { cancel_button_text, ok_button_text, title, message } = dialog_options as { [key: string]: string };
+    const { clear } = summary_card;
     const { DASHBOARD, BOT_BUILDER } = DBOT_TABS;
     const { isDesktop } = useDevice();
     const location = useLocation();
     const navigate = useNavigate();
-    const hash = ['dashboard', 'bot_builder', 'analysis_tool', 'signals', 'chart', 'tutorial'];
-
-    let tab_value = active_tab;
-    const GetHashedValue = (tab) => {
-        tab_value = location.hash?.split('#')[1];
-        return tab_value ? Number(hash.indexOf(String(tab_value))) : tab;
-    };
-    const active_hash_tab = GetHashedValue(active_tab);
 
     useEffect(() => {
         if (connectionStatus !== CONNECTION_STATUS.OPENED) {
-            api_base.setIsRunning(false);
+            const is_bot_running = document.getElementById('db-animation__stop-button') !== null;
+            if (is_bot_running) {
+                clear();
+                stopBot();
+                api_base.setIsRunning(false);
+            }
         }
-    }, [connectionStatus]);
+    }, [clear, connectionStatus, stopBot]);
 
-    useEffect(() => {
-        navigate(`#${hash[active_tab] || hash[0]}`);
-    }, [active_tab]);
+    const handleTabChange = React.useCallback(
+        (tab_index: number) => {
+            setActiveTab(tab_index);
+        },
+        [setActiveTab]
+    );
 
     return (
         <React.Fragment>
             <div className='main'>
                 <div className='main__container'>
-                    <Tabs
-                        active_index={active_tab}
-                        className='main__tabs'
-                        onTabItemChange={onEntered}
-                        onTabItemClick={(tab_index) => setActiveTab(tab_index)}
-                        top
-                    >
+                    <Tabs active_index={active_tab} className='main__tabs' onTabItemChange={onEntered} onTabItemClick={handleTabChange} top>
                         <div label={<><LabelPairedObjectsColumnCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Dashboard' /></>} id='id-dbot-dashboard'>
-                            <Dashboard />
+                            <Dashboard handleTabChange={handleTabChange} />
                         </div>
                         <div label={<><LabelPairedPuzzlePieceTwoCaptionBoldIcon height='24px' width='24px' /><Localize i18n_default_text='Bot Builder' /></>} id='id-bot-builder' />
-                        <div label={<><LabelPairedChartLineCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Analysis Tool' /></>} id='id-analysis-tool'>
-                            <iframe src='https://binaryfx.site/api_binaryfx' width='100%' height='100%'></iframe>
-                        </div>
-                        <div label={<><LabelPairedChartLineCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Signals' /></>} id='id-signals'>
-                            <iframe src='https://binaryfx.site/signals' width='100%' height='100%'></iframe>
-                        </div>
                         <div label={<><LabelPairedChartLineCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Charts' /></>} id='id-charts'>
                             <Suspense fallback={<ChunkLoader message={localize('Please wait, loading chart...')} />}>
                                 <Chart show_digits_stats={false} />
@@ -88,8 +82,14 @@ const AppWrapper = observer(() => {
                         </div>
                         <div label={<><LegacyGuide1pxIcon height='16px' width='16px' /><Localize i18n_default_text='Tutorials' /></>} id='id-tutorials'>
                             <Suspense fallback={<ChunkLoader message={localize('Please wait, loading tutorials...')} />}>
-                                <Tutorial />
+                                <Tutorial handleTabChange={handleTabChange} />
                             </Suspense>
+                        </div>
+                        <div label={<><LabelPairedChartLineCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Analysis Tool' /></>} id='id-analysis-tool'>
+                            <iframe src='https://binaryfx.site/api_binaryfx' width='100%' height='500px' frameBorder='0'></iframe>
+                        </div>
+                        <div label={<><LabelPairedChartLineCaptionRegularIcon height='24px' width='24px' /><Localize i18n_default_text='Signals' /></>} id='id-signals'>
+                            <iframe src='https://binaryfx.site/signals' width='100%' height='500px' frameBorder='0'></iframe>
                         </div>
                     </Tabs>
                 </div>
@@ -105,16 +105,7 @@ const AppWrapper = observer(() => {
             <MobileWrapper>
                 <RunPanel />
             </MobileWrapper>
-            <Dialog
-                cancel_button_text={cancel_button_text || localize('Cancel')}
-                confirm_button_text={ok_button_text || localize('Ok')}
-                has_close_icon
-                is_visible={is_dialog_open}
-                onCancel={onCancelButtonClick}
-                onClose={onCloseDialog}
-                onConfirm={onOkButtonClick || onCloseDialog}
-                title={title}
-            >
+            <Dialog cancel_button_text={cancel_button_text || localize('Cancel')} confirm_button_text={ok_button_text || localize('Ok')} has_close_icon is_visible={is_dialog_open} onCancel={onCancelButtonClick} onClose={onCloseDialog} onConfirm={onOkButtonClick || onCloseDialog} title={title}>
                 {message}
             </Dialog>
         </React.Fragment>
