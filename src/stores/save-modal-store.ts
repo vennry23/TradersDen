02c1db763790c35fc131156c40a3c4dc3308503e
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-03-14 22:39:09 +0300

    Update save-modal-store.ts

--- src/stores/save-modal-store.ts
+++ src/stores/save-modal-store.ts
@@ -59,17 +59,14 @@ export default class SaveModalStore implements ISaveModalStore {
         if (!this.is_save_modal_open) {
             this.setButtonStatus(button_status.NORMAL);
         }
-
         this.is_save_modal_open = !this.is_save_modal_open;
     };
 
     validateBotName = (values: string): { [key: string]: string } => {
         const errors = {};
-
-        if (values.bot_name.trim() === '') {
+        if (values.trim() === '') {
             errors.bot_name = localize('Strategy name cannot be empty');
         }
-
         return errors;
     };
 
@@ -81,17 +78,26 @@ export default class SaveModalStore implements ISaveModalStore {
         const { saveFile } = google_drive;
         let xml;
         let main_strategy = null;
+
         if (active_tab === 1) {
-            xml = window.Blockly?.Xml?.workspaceToDom(window.Blockly?.derivWorkspace);
+            xml = Blockly?.Xml?.workspaceToDom(Blockly?.derivWorkspace);
         } else {
             const recent_files = await getSavedWorkspaces();
-            main_strategy = recent_files.filter((strategy: TStrategy) => strategy.id === selected_strategy.id)?.[0];
-            main_strategy.name = bot_name;
-            main_strategy.save_type = is_local ? save_types.LOCAL : save_types.GOOGLE_DRIVE;
-            xml = window.Blockly.utils.xml.textToDom(main_strategy.xml);
+            main_strategy = recent_files.find((strategy: TStrategy) => strategy.id === selected_strategy.id);
+            if (main_strategy) {
+                main_strategy.name = bot_name;
+                main_strategy.save_type = is_local ? save_types.LOCAL : save_types.GOOGLE_DRIVE;
+                xml = Blockly.utils.xml.textToDom(main_strategy.xml);
+            }
         }
-        xml.setAttribute('is_dbot', 'true');
-        xml.setAttribute('collection', save_as_collection ? 'true' : 'false');
+
+        if (xml instanceof Element) {
+            xml.setAttribute('is_dbot', 'true');
+            xml.setAttribute('collection', save_as_collection ? 'true' : 'false');
+        } else {
+            console.error('Invalid XML structure:', xml);
+            return;
+        }
 
         const customContent = Blockly?.Xml?.domToPrettyText(xml);
         const customFileFormat = `<!CUSTOM_FORMAT_START>${customContent}<!CUSTOM_FORMAT_END>`;
@@ -108,9 +114,8 @@ export default class SaveModalStore implements ISaveModalStore {
         }
 
         this.updateBotName(bot_name);
-
         if (active_tab === 0) {
-            const workspace_id = selected_strategy.id ?? Blockly?.utils?.genUid();
+            const workspace_id = selected_strategy?.id ?? Blockly?.utils?.genUid();
             await this.addStrategyToWorkspace(workspace_id, is_local, save_as_collection, bot_name, xml);
             if (main_strategy) await loadStrategyToBuilder(main_strategy);
         } else {
@@ -125,7 +130,6 @@ export default class SaveModalStore implements ISaveModalStore {
 
     onDriveConnect = async () => {
         const { google_drive } = this.root_store;
-
         if (google_drive.is_authorised) {
             google_drive.signOut();
         } else {
