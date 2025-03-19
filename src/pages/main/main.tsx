e699216dc6391d3fc8f6ec527c42a32c2bd82be5
Machariarobert <132147745+Machariarobert@users.noreply.github.com>
2025-03-20 00:28:13 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -32,7 +32,7 @@ const DashboardIcon = () => (
 
 const BotBuilderIcon = () => (
     <svg width="24" height="24" fill="var(--text-general)" viewBox="0 0 24 24">
-        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" />
+        <path d="M19 13h-6v-2h6v2zm-8 0H5v-2h6v2zm8-4h-6V7h6v2zm-8 0H5V7h6v2zm8-4h-6V3h6v2zm-8 0H5V3h6v2zm10 14H3v2h18v-2zm0-4H3v2h18v-2zm0-4H3v2h18v-2zm0-4H3v2h18V7zm0-4H3v2h18V3z" />
     </svg>
 );
 
