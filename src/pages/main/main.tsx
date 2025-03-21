33fb26de54b01099366118479a7731a4b79bf166
machariarobert894 <gloriasoniah84@gmail.com>
2025-03-22 02:50:31 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -68,7 +68,12 @@ const TradingHubIcon = () => (
 );
 
 const FreeBotsIcon = () => (
-    <svg width="20px" height="20px" viewBox="0 0 24 24" data-name="025_SCIENCE" id="_025_SCIENCE" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:"var(--text-general)";}</style></defs><path class="cls-1" d="M18,22H6a3,3,0,0,1-3-3V7A3,3,0,0,1,6,4H18a3,3,0,0,1,3,3V19A3,3,0,0,1,18,22ZM6,6A1,1,0,0,0,5,7V19a1,1,0,0,0,1,1H18a1,1,0,0,0,1-1V7a1,1,0,0,0-1-1Z"/><path class="cls-1" d="M9,13a1,1,0,0,1-1-1V10a1,1,0,0,1,2,0v2A1,1,0,0,1,9,13Z"/><path class="cls-1" d="M15,13a1,1,0,0,1-1-1V10a1,1,0,0,1,2,0v2A1,1,0,0,1,15,13Z"/><path class="cls-1" d="M12,5a1,1,0,0,1-1-1V2a1,1,0,0,1,2,0V4A1,1,0,0,1,12,5Z"/><path class="cls-1" d="M15,18H9a1,1,0,0,1,0-2h6a1,1,0,0,1,0,2Z"/></svg>
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
+<polygon points="22,16.8 22,26 6,26 6,10 15.2,10 17.2,8 4,8 4,28 24,28 24,14.8 "/>
+<path fill="none" stroke="var(--text-general)" stroke-width="2" stroke-miterlimit="10" d="M16.5,18.3L13,19l0.7-3.5l9.9-9.9
+	c0.8-0.8,2-0.8,2.8,0l0,0c0.8,0.8,0.8,2,0,2.8L16.5,18.3z"/>
+</svg>
 );
 
 const AppWrapper = observer(() => {
