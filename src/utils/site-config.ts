cc2667471ae2b0834093c5b323cd0463a92ebff8
machariarobert894 <gloriasoniah84@gmail.com>
2025-03-21 23:49:38 +0300

    Update site-config.ts

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
-export const website_domain = 'bot.binaryfx.site';
-export const website_name = 'Binaryfx';
+export const website_domain = 'bot.derivlite.com';
+export const website_name = 'Derivlite';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
