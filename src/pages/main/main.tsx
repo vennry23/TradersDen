2773e60976b7ba02cedd40c6c1ef4147f69007da
machariarobert894 <gloriasoniah84@gmail.com>
2025-03-22 02:55:30 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -68,12 +68,9 @@ const TradingHubIcon = () => (
 );
 
 const FreeBotsIcon = () => (
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-	 viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
-<polygon points="22,16.8 22,26 6,26 6,10 15.2,10 17.2,8 4,8 4,28 24,28 24,14.8 "/>
-<path fill="none" stroke="var(--text-general)" stroke-width="2" stroke-miterlimit="10" d="M16.5,18.3L13,19l0.7-3.5l9.9-9.9
-	c0.8-0.8,2-0.8,2.8,0l0,0c0.8,0.8,0.8,2,0,2.8L16.5,18.3z"/>
-</svg>
+    <svg width="24" height="24" fill="var(--text-general)" viewBox="0 0 24 24">
+        <path d="M2 21h19v-3H2v3zM20 3H4v10l8 5 8-5V3zm-8 4.5c-1.11 0-2-.89-2-2s.89-2 2-2 2 .89 2 2-.89 2-2 2z" />
+    </svg>
 );
 
 const AppWrapper = observer(() => {
