caf7fddae5249d015e00aa54abaff8d56c22ee50
wallace guthera <machariarobert894@gmail.com>
2025-03-24 19:53:57 +0300

    .

--- src/constants/bot-contents.ts
+++ src/constants/bot-contents.ts
@@ -16,10 +16,13 @@ export const DBOT_TABS: TDashboardTabIndex = Object.freeze({
     BOT_BUILDER: 1,
     CHART: 2,
     TUTORIAL: 3,
+    ANALYSIS_TOOL: 4,
+    SIGNALS: 5,
+
 });
 
 export const MAX_STRATEGIES = 10;
 
-export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-charts', 'id-tutorials'];
+export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-charts', 'id-tutorials', 'id-analysis-tool', 'id-signals'];
 
 export const DEBOUNCE_INTERVAL_TIME = 500;
