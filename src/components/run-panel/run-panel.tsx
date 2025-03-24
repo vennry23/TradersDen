cec32066e69baa71bf96cf268473f758e273c0ad
wallace guthera <machariarobert894@gmail.com>
2025-03-24 16:10:53 +0300

    .

--- src/components/run-panel/run-panel.tsx
+++ src/components/run-panel/run-panel.tsx
@@ -253,7 +253,7 @@ const RunPanel = observer(() => {
     const { statistics } = transactions;
     const { active_tour, active_tab } = dashboard;
     const { total_payout, total_profit, total_stake, won_contracts, lost_contracts, number_of_runs } = statistics;
-    const { BOT_BUILDER, CHART } = DBOT_TABS;
+    const { BOT_BUILDER, CHART, SIGNALS, ANALYSIS_TOOL } = DBOT_TABS;
 
     React.useEffect(() => {
         onMount();
@@ -296,7 +296,7 @@ const RunPanel = observer(() => {
         />
     );
 
-    const show_run_panel = [BOT_BUILDER, CHART].includes(active_tab) || active_tour;
+    const show_run_panel = [BOT_BUILDER, CHART, SIGNALS, ANALYSIS_TOOL].includes(active_tab) || active_tour;
     if ((!show_run_panel && isDesktop) || active_tour === 'bot_builder') return null;
 
     return (
