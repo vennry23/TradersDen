ece01540896e64c060154bc945537d825180cdc2
wallace guthera <machariarobert894@gmail.com>
2025-03-31 02:48:03 +0300

    Revert "."
    This reverts commit fd02b048b18486f3d3f3de8a2ec99d0de015152f.

--- src/external/bot-skeleton/utils/index.js
+++ src/external/bot-skeleton/utils/index.js
@@ -4,26 +4,12 @@ export { timeSince } from './date-time-helper';
 export { createError, trackAndEmitError } from './error';
 export { handleError, initErrorHandlingListener, removeErrorHandlingEventListener } from './error-handling';
 export { importExternal } from './html-helper';
-export { getSavedWorkspaces, removeExistingWorkspace, saveWorkspaceToRecent, convertStrategyToIsDbot } from './local-storage';
+export {
+    convertStrategyToIsDbot,
+    getSavedWorkspaces,
+    removeExistingWorkspace,
+    saveWorkspaceToRecent,
+} from './local-storage';
 export { observer } from './observer';
 export { compareXml, extractBlocksFromXml, pipe, sortBlockChild } from './strategy-helper';
-export { LogTypes, runReport } from './trade-reporter';
-export {
-    addDomAsBlock,
-    cleanUpOnLoad,
-    delayExecution,
-    disable,
-    download,
-    enable,
-    excludeOptionFromContextMenu,
-    isCustomDialog,
-    isDialogType,
-    isMainBlock,
-    load,
-    loadBlocks,
-    loadWorkspace,
-    notify,
-    save,
-    showIncompatibleStrategyDialog,
-} from './utils';
 export { onWorkspaceResize } from './workspace';
