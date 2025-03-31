73d41e1c28286848ccff49f4d41e28215c995027
wallace guthera <machariarobert894@gmail.com>
2025-03-31 03:55:04 +0300

    Revert "."
    This reverts commit c2351f246f6503bd659c7e5eb550577aeb350dee.

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -29,13 +29,6 @@ const DashboardComponent = observer(({ handleTabChange }: TMobileIconGuide) => {
                     'tab__dashboard--tour-active': active_tour,
                 })}
             >
-                <div className='tab__dashboard__header'>
-                    <ToggleSwitch
-                        label={localize('Link signals to trading workflow')}
-                        isChecked={is_link_signals_to_workflow}
-                        onChange={setLinkSignalsToWorkflow}
-                    />
-                </div>
                 <div className='tab__dashboard__content'>
                     <Announcements is_mobile={!isDesktop} is_tablet={isTablet} handleTabChange={handleTabChange} />
                     <div className='quick-panel'>
@@ -44,6 +37,11 @@ const DashboardComponent = observer(({ handleTabChange }: TMobileIconGuide) => {
                                 'tab__dashboard__header--listed': isDesktop && has_dashboard_strategies,
                             })}
                         >
+                            <ToggleSwitch
+                                label={localize('Link signals to trading workflow')}
+                                isChecked={is_link_signals_to_workflow}
+                                onChange={setLinkSignalsToWorkflow}
+                            />
                             {!has_dashboard_strategies && (
                                 <Text
                                     className='title'
