c61dd33d498e8c2451ed6e0354b1af9bb3b1f63b
wallace guthera <machariarobert894@gmail.com>
2025-03-31 04:32:48 +0300

    Revert "."
    This reverts commit 605df3338e85dc2487aac26dfb698e3baddd7af5.

--- src/signals/signals.js
+++
@@ -1,8 +0,0 @@
-export const fetchSignals = async () => {
-    // Example: Replace with actual signal fetching logic
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(['Signal 1', 'Signal 2', 'Signal 3']);
-        }, 1000);
-    });
-};
