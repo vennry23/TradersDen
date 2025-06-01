89be7d59d01c6e8c241b584bb9c283036e811470
mesoflix <mesoflixltd@gmail.com>
2025-06-01 06:12:54 +0300

    Update ai.js

--- public/ai/ai.js
+++ public/ai/ai.js
@@ -12,7 +12,7 @@ function startWebSocket() {
         derivWs.close();
     }
 
-    derivWs = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
+    derivWs = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=80058');
 
     derivWs.onopen = function () {
         requestTickHistory();
