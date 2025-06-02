1d4d641459f6f35fd7b917de7896014fcf728dae
mesoflix <mesoflixltd@gmail.com>
2025-06-02 03:10:13 +0300

    Update analysis.tsx

--- src/components/analysistool/analysis.tsx
+++ src/components/analysistool/analysis.tsx
@@ -3,7 +3,7 @@ const AnalysistoolComponent = () => {
         <iframe
             id='trading-view-iframe'
             style={{ width: '100%', height: '100%', backgroundColor: 'white' }}
-            src='https://binaryfx.site/x-bot?hide-signup=true'
+            src='https://autos.marketmovers.life'
         />
     );
 };
