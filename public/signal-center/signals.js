2b6ca7471695b8c1fec240094f2138bc7e3836df
vennry23 <lemicmelic@gmail.com>
2025-06-23 01:14:22 +1100

    Update signals.js

--- public/signal-center/signals.js
+++ public/signal-center/signals.js
@@ -11,7 +11,7 @@ let isTrading = false;
 let oauthToken = null;
 let tradeHistory = [];
 
-const ws = new WebSocket('wss://ws.derivws.com/websockets/v3?app_id=80058');
+const ws = new WebSocket('wss://ws.derivws.com/websockets/v3?app_id=82255');
 
 ws.onopen = () => {
     Object.keys(ticksStorage).forEach(subscribeTicks);
