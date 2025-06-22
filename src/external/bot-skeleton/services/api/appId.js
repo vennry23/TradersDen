cd879425a384e0540aaf829f18594852bcbc709b
vennry23 <lemicmelic@gmail.com>
2025-06-23 01:16:12 +1100

    Update appId.js

--- src/external/bot-skeleton/services/api/appId.js
+++ src/external/bot-skeleton/services/api/appId.js
@@ -6,7 +6,7 @@ import APIMiddleware from './api-middleware';
 
 export const generateDerivApiInstance = () => {
     const cleanedServer = getSocketURL().replace(/[^a-zA-Z0-9.]/g, '');
-    const cleanedAppId = '80058';
+    const cleanedAppId = '82255';
     const socket_url = `wss://ws.binaryws.com/websockets/v3?app_id=${cleanedAppId}&l=${getInitialLanguage()}&brand=${website_name.toLowerCase()}`;
     const deriv_socket = new WebSocket(socket_url);
     const deriv_api = new DerivAPIBasic({
